cce60ccb0209cb4aa570c2ce603d5d1ee8a3c6b3
Phung Phu <phuphung123789@gmail.com>
2024-10-15 09:51:07 +0700

    redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import store, { persistor } from './redux/store'
+
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <StrictMode>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </StrictMode>,
   </Provider>
 
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,14 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import rootReducer from "../reducer/rootReducer";
 import userSlice from "./slices/userSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['value']
+    //chỉ lưu giá trị của value. 
+    //sau khi em f5 thì bình thường nó phải mất dữ liệu nhưng giờ nó không mất
+}
+
+const persistedReducer = persistReducer(persistConfig, userSlice)
 
 const store = configureStore(
     {
         reducer: {
-            user: userSlice
+            user: persistedReducer
         }
     }
 )
 
+export const persistor = persistStore(store)
 export default store
